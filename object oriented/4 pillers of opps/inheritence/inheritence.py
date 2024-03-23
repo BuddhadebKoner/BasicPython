658655a68735d10e617220d3c52b916a34d669db
buddhadeb <jeetkoner36@gmail.com>
2024-03-23 14:04:17 +0530

    inheritence.py

--- object oriented/4 pillers of opps/inheritence/inheritence.py
+++ object oriented/4 pillers of opps/inheritence/inheritence.py
@@ -1,4 +1,31 @@
 """
-when one class (child/deriven) derives the property & methods of another class (parent/base)
+when one class (child/derive) derives the property & methods of another class (parent/base)
 """
+
+
+class Person:
+    def __init__(self, fame, name):
+        self.firstname = fame
+        self.lastname = name
+
+    def printname(self):  # this is the syntax to inherit the
+        print(self.firstname, self.lastname)
+
+
+# Use the Person class to create an object, and then execute the printname method:
+
+x = Person("ms", "dhoni")
+x.printname()
+
+
+# Create a class named Student, which will inherit the properties and methods from the Person class:
+class Student(Person):  # child class (parents class)
+    pass
+
+
+# Use the Student class to create an object, and then execute the printname method:
+x = Student("virat", "koholi")
+x.printname()
+
+
 
--- object oriented/Basic opps/methods.py
+++ object oriented/Basic opps/methods.py
@@ -1,6 +1,8 @@
 '''
 In a class, all functions are called methods.
 '''
+
+
 # NORMAL METHODS --->
 
 class Methods:
@@ -9,30 +11,32 @@ class Methods:
         self.value = value
         print("The value is:", self.value)
 
-    def welcome(self): # this is object-level method, that's why it has a self parameter
+    def welcome(self):  # this is object-level method, that's why it has a self parameter
         print('Welcome here')
+
 
 obj1 = Methods(10)
 obj1.welcome()
-
 
 '''
 Create a Student class that takes name & marks of 3 subjects as arguments in the constructor, then create a method to print the average.
 '''
 
+
 class Student:
     def __init__(self, name, marks):
         self.name = name
         self.marks = marks
-        
+
     def get_avg(self):
         total = sum(self.marks)
         num_subjects = len(self.marks)
         print("Hi", self.name, "Your Average is:", total / num_subjects)
-                    
+
+
 s1 = Student("Tony Stark", [99, 76, 54, 100])
 s1.get_avg()
-        
+
 s1.name = "Iron Man"  # You can change the value of attributes
 s1.get_avg()
 
@@ -42,16 +46,16 @@ s1.get_avg()
 Methods Are dont use self parameter(work at class lavel not obj lavel) 
 '''
 
+
 class Car:
-    
-    @staticmethod # decorater which is contvert to a static methods 
-    # "decoratera" allow to wrap another function in order to extended the behaviour of warpped function , without permentaly modifying it
+
+    @staticmethod  # decorator which is convert to a static methods
+    # "decorator" allow to wrap another function in order to extend the behaviour of warped function ,
+    # without permanently modifying it
     def print_hello():
         print("hello")
-    
-    
+
     def __init__(self, make, model, year):
         self.make = make
         self.model = model
         self.year = year
-        
